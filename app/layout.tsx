3124c7e8a42a38573918dc8f91b323180289b660
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2026-09-24 07:57:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
         <Analytics />
-        <SpeedInsights />
+        <SpeedInsights / >
       </body>
     </html>
   );
